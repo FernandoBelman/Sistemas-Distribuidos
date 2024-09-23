17fc151bd62915b40942e3a9fdfe410ca32d62c0
FernandoBelman <lbelman08@alumnos.uaq.mx>
2024-09-23 10:22:24 -0600

    Resolver conflictos

--- FileDownload/obj/Debug/net8.0/FileDownload.AssemblyInfo.cs
+++ FileDownload/obj/Debug/net8.0/FileDownload.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RestApi")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FileDownload")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49e3589046af7fb312d880d58d069be87a1f7e6")]
-[assembly: System.Reflection.AssemblyProductAttribute("RestApi")]
-[assembly: System.Reflection.AssemblyTitleAttribute("RestApi")]
+[assembly: System.Reflection.AssemblyProductAttribute("FileDownload")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FileDownload")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RestApi")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49e3589046af7fb312d880d58d069be87a1f7e6")]
-[assembly: System.Reflection.AssemblyProductAttribute("RestApi")]
-[assembly: System.Reflection.AssemblyTitleAttribute("RestApi")]
+[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
