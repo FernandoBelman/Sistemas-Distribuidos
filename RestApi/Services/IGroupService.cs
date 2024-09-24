9830a064243ba8b215cbc8902fec01882a114b21
FernandoBelman <lbelman08@alumnos.uaq.mx>
2024-09-24 14:37:32 -0600

    feat: Paginacion y Agregacion

--- RestApi/Services/IGroupService.cs
+++ RestApi/Services/IGroupService.cs
@@ -4,8 +4,6 @@ namespace RestApi.Services;
 
 public interface IGroupService{
     Task<GroupUserModel> GetGroupByIdAsync (string Id, CancellationToken cancellationToken);
-
-    Task<IEnumerable<GroupUserModel>> GetGroupsByNameAsync(string name, CancellationToken cancellationToken);
-
+    Task<IEnumerable<GroupUserModel>> GetGroupsByNameAsync(string name, int pageIndex, int pageSize, string orderBy, CancellationToken cancellationToken); 
 
 }
